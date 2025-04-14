cd56ee40d303bdb0dc3d684bfb666b48ce484ca9
OoiWeiSian <weisianooi@gmail.com>
2025-04-15 01:19:36 +0800

    Modify

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+import checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// ----------------------------------------------------------------------
+
+const PORT = 3000;
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+        dev: { logLevel: ['error'] },
+      },
+      overlay: {
+        position: 'tl',
+        initialIsOpen: false,
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: /^src(.+)/,
+        replacement: path.resolve(process.cwd(), 'src/$1'),
+      },
+    ],
+  },
+  server: {
+    port: PORT,
+    host: 'localhost', // Bind to localhost
+    open: true, // Automatically open the browser
+  },
+  preview: {
+    port: PORT,
+    host: 'localhost',
+  },
+});
